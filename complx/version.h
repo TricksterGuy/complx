7b5f2690a4e665c111f23b3117d8c00a711350d8
Brandon <tricksterguy@live.com>
2015-02-05 00:05:50 -0800

    Version 4.7.0

--- complx/version.h
+++ complx/version.h
@@ -4,10 +4,10 @@
 namespace AutoVersion{
 
 	//Date Version Types
-	static const char DATE[] = "20";
-	static const char MONTH[] = "9";
-	static const char YEAR[] = "2013";
-	static const char UBUNTU_VERSION_STYLE[] = "13.9";
+	static const char DATE[] = "5";
+	static const char MONTH[] = "2";
+	static const char YEAR[] = "2015";
+	static const char UBUNTU_VERSION_STYLE[] = "15.2";
 
 	//Software Status
 	static const char STATUS[] = "Release";
@@ -15,15 +15,15 @@ namespace AutoVersion{
 
 	//Standard Version Type
 	static const long MAJOR = 4;
-	static const long MINOR = 6;
+	static const long MINOR = 7;
 	static const long BUILD = 0;
 	static const long REVISION = 0;
 
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT = 0;
-	#define RC_FILEVERSION 4,6,0,0
-	#define RC_FILEVERSION_STRING "4, 6, 0, 0\0"
-	static const char FULLVERSION_STRING[] = "4.6.0.0";
+	#define RC_FILEVERSION 4,7,0,0
+	#define RC_FILEVERSION_STRING "4, 7, 0, 0\0"
+	static const char FULLVERSION_STRING[] = "4.7.0.0";
 
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY = 0;
