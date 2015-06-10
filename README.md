f92ada827cf3fdd46bf7155da6b46fcc39e4fcad
Brandon <tricksterguy@live.com>
2015-06-10 02:21:09 -0700

    Version bump 4.8.0.0; Finalize documentation; Open floodgates

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ complx
 
 Complx the LC-3 Simulator used in CS2110 at Georgia Tech currently maintained and authored by Brandon (bwhitehead0308 [AT] gmail [DOT] com)
 
-Repository will be ready for Summer 2015 class on Tuesday June 9th 2015. If you are a Summer 2015 CS2110 student come back on Tuesday.
+Repository ready for Summer 2015 CS2110 Class.
+
+Please refer to complx-tools.pdf on information on how to install this set of tools.
--- complx/version.h
+++ complx/version.h
@@ -4,10 +4,10 @@
 namespace AutoVersion {
 
 	//Date Version Types
-	static const char DATE[] = "3";
-	static const char MONTH[] = "5";
+	static const char DATE[] = "10";
+	static const char MONTH[] = "6";
 	static const char YEAR[] = "2015";
-	static const char UBUNTU_VERSION_STYLE[] = "15.5";
+	static const char UBUNTU_VERSION_STYLE[] = "15.6";
 
 	//Software Status
 	static const char STATUS[] = "Release";
@@ -15,15 +15,15 @@ namespace AutoVersion {
 
 	//Standard Version Type
 	static const long MAJOR = 4;
-	static const long MINOR = 8;
+	static const long MINOR = 9;
 	static const long BUILD = 0;
 	static const long REVISION = 0;
 
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT = 0;
-	#define RC_FILEVERSION 4,8,0,0
-	#define RC_FILEVERSION_STRING "4, 8, 0, 0\0"
-	static const char FULLVERSION_STRING[] = "4.8.0.0";
+	#define RC_FILEVERSION 4,9,0,0
+	#define RC_FILEVERSION_STRING "4, 9, 0, 0\0"
+	static const char FULLVERSION_STRING[] = "4.9.0.0";
 
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY = 0;
