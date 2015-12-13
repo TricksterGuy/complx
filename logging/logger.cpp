556c76b6544c92b18a722587f6820b7ae7da09bd
Brandon <tricksterguy@live.com>
2015-12-13 13:36:07 -0800

    #32 At least add the logging files

---
+++ logging/logger.cpp
@@ -0,0 +1,110 @@
+#include "logger.hpp"
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+
+std::string time_to_string(std::chrono::system_clock::time_point& time_point)
+{
+    std::time_t time_secs = std::chrono::system_clock::to_time_t(time_point);
+    std::chrono::system_clock::time_point time_point_sec = std::chrono::system_clock::from_time_t(time_secs);
+    std::chrono::milliseconds ms = std::chrono::duration_cast<std::chrono::milliseconds>(time_point - time_point_sec);
+
+    char buffer[128];
+    strftime(buffer, 128, "%H:%M:%S", localtime(&time_secs));
+    char currentTime[128] = "";
+    snprintf(currentTime, 128, "%s.%03d", buffer, (int)ms.count());
+
+    return currentTime;
+}
+
+std::unique_ptr<AbstractLogger> logger(new Logger());
+
+void SetLogger(AbstractLogger* logobj)
+{
+    logger.reset(logobj);
+}
+
+inline const char* GetLogAbbrev(LogLevel level)
+{
+    switch(level)
+    {
+        case LogLevel::FATAL:
+            return "F";
+        case LogLevel::DEBUG:
+            return "D";
+        case LogLevel::WARNING:
+            return "W";
+        case LogLevel::INFO:
+            return "I";
+        case LogLevel::VERBOSE:
+            return "V";
+        default:
+            return "?";
+    }
+}
+
+inline const char* GetLogColor(LogLevel level)
+{
+#ifndef WINDOWS
+    switch(level)
+    {
+        case LogLevel::FATAL:
+            return "\033[1;31m";
+        case LogLevel::DEBUG:
+            return "\033[1;33m";
+        case LogLevel::WARNING:
+            return "\033[1;33m";
+        case LogLevel::INFO:
+            return "";
+        case LogLevel::VERBOSE:
+            return "\033[2;34m";
+        default:
+            return "";
+    }
+#else
+    return "";
+#endif
+}
+
+inline const char* EndLogColor()
+{
+#ifndef WINDOWS
+    return "\033[0m";
+#else
+    return "";
+#endif
+}
+
+void AbstractLogger::Log(LogLevel level, const char* format, va_list ap)
+{
+    if (level > log_level)
+        return;
+
+    if (log_time)
+    {
+        std::chrono::time_point<std::chrono::system_clock> time_now(std::chrono::system_clock::now());
+        (*out) << GetLogColor(level) << GetLogAbbrev(level) << "[" << time_to_string(time_now) << "]" << EndLogColor();
+    }
+
+    DoLog(level, format, ap);
+}
+
+void Logger::DoLog(LogLevel level, const char* format, va_list ap)
+{
+    char buffer[1024];
+    vsnprintf(buffer, 1024, format, ap);
+    (*out) << buffer << std::endl;
+    if (level == LogLevel::FATAL) exit(EXIT_FAILURE);
+}
+
+EventLog::EventLog(const char* function) : func(function), startTime(std::chrono::system_clock::now())
+{
+    VerboseLog("start: %s", func);
+}
+
+EventLog::~EventLog()
+{
+    std::chrono::time_point<std::chrono::system_clock> endTime(std::chrono::system_clock::now());
+    std::chrono::duration<double> elapsed_seconds = endTime-startTime;
+    VerboseLog("end: %s time: %fs", func, elapsed_seconds.count());
+}
---
+++ logging/logger.hpp
@@ -0,0 +1,108 @@
+#ifndef LOGGER_HPP
+#define LOGGER_HPP
+
+#include <iostream>
+#include <cstdarg>
+#include <memory>
+#include <chrono>
+
+enum class LogLevel
+{
+    FATAL = 0,   // printed and stops program
+    DEBUG = 1,
+    WARNING = 2,
+    INFO = 3,
+    VERBOSE = 4,
+};
+
+class AbstractLogger
+{
+    public:
+        AbstractLogger(std::ostream* target = &std::cerr) : out(target), log_level(LogLevel::INFO), log_time(true) {}
+        virtual ~AbstractLogger() {}
+        virtual void Log(LogLevel level, const char* format, va_list ap);
+        virtual void DoLog(LogLevel level, const char* format, va_list ap) {}
+        void SetLogTarget(std::ostream* stream) {out = stream;}
+        void SetLogLevel(LogLevel level) {log_level = level;}
+        void SetLogTime(bool logging_time) {log_time = logging_time;}
+    protected:
+        std::ostream* out;
+        LogLevel log_level;
+        bool log_time;
+};
+
+class Logger : public AbstractLogger
+{
+    public:
+        virtual void DoLog(LogLevel level, const char* format, va_list ap);
+};
+
+extern std::unique_ptr<AbstractLogger> logger;
+
+void SetLogger(AbstractLogger* logobj);
+
+static inline void Log(LogLevel level, const char* format, ...)
+{
+    va_list argptr;
+    va_start(argptr, format);
+    logger->Log(level, format, argptr);
+    va_end(argptr);
+}
+
+static inline void Log(LogLevel level, const char* format, va_list arg)
+{
+    logger->Log(level, format, arg);
+}
+
+static inline void FatalLog(const char* format, ...)
+{
+    va_list argptr;
+    va_start(argptr, format);
+    Log(LogLevel::FATAL, format, argptr);
+    va_end(argptr);
+}
+
+static inline void DebugLog(const char* format, ...)
+{
+    va_list argptr;
+    va_start(argptr, format);
+    Log(LogLevel::DEBUG, format, argptr);
+    va_end(argptr);
+}
+
+static inline void WarnLog(const char* format, ...)
+{
+    va_list argptr;
+    va_start(argptr, format);
+    Log(LogLevel::WARNING, format, argptr);
+    va_end(argptr);
+}
+
+static inline void InfoLog(const char* format, ...)
+{
+    va_list argptr;
+    va_start(argptr, format);
+    Log(LogLevel::INFO, format, argptr);
+    va_end(argptr);
+}
+
+static inline void VerboseLog(const char* format, ...)
+{
+    va_list argptr;
+    va_start(argptr, format);
+    Log(LogLevel::VERBOSE, format, argptr);
+    va_end(argptr);
+}
+
+/** Object that only exists to print out start and end of event call in a function */
+class EventLog
+{
+    public:
+        EventLog(const char* function);
+        ~EventLog();
+    private:
+        const char* func;
+        std::chrono::time_point<std::chrono::system_clock> startTime;
+};
+
+#endif
