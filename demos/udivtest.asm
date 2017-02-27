597bc827e811d9836812261c10734d72b54f12e9
Brandon <TricksterGuy@users.noreply.github.com>
2017-02-26 19:14:10 -0800

    Punctuation and grammar

--- demos/udivtest.asm
+++ demos/udivtest.asm
@@ -2,8 +2,8 @@
 
 ; Demo that shows how to divide two numbers using UDIV.
 ; UDIV preconditions R0 = A, R1 = B
-; UDIV postconditions R0 = A / B R1 = A % B
-; CC is unchanged, % has the same behaviour as C in Java in regards to negative numbers
+; UDIV postconditions R0 = A / B, R1 = A % B
+; CC is unchanged, % result has the same behaviour as C and Java in regards to negative numbers
 .orig x3000
 	LD R0, A
 	LD R1, B
