613c807ffdc187a906340d83e3426fb5f065fe0b
Brandon <TricksterGuy@users.noreply.github.com>
2017-10-24 00:08:31 -0400

    Update udivtest.asm

--- demos/udivtest.asm
+++ demos/udivtest.asm
@@ -1,6 +1,7 @@
 ;@plugin filename=lc3_udiv vector=x80
 
 ; Demo that shows how to divide two numbers using UDIV.
+; UDIV is a custom trap implemented in C++ and will be available at trap vector x80.
 ; UDIV preconditions R0 = A, R1 = B
 ; UDIV postconditions R0 = A / B, R1 = A % B
 ; CC is unchanged, % result has the same behaviour as C and Java in regards to negative numbers
