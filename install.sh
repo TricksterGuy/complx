c85fbbfb909fe6903e446ec1b4f3422874b3fe43
Cem Gökmen <skyman@users.noreply.github.com>
2018-09-18 17:02:19 -0700

    Include requirements

--- install.sh
+++ install.sh
@@ -27,6 +27,9 @@ echo >&2 "Installing Boost Python"
 apt-get install -y libboost-python-dev > /dev/null
 echo >&2 "Installing Glib 2.0"
 apt-get install -y libglib2.0-dev > /dev/null
+echo >&2 "Installing required Python libraries"
+apt-get install python-pip > /dev/null
+pip install -r pylc3/requirements.txt > /dev/null
 echo >&2 "Creating build directory"
 mkdir -p build
 cd build
