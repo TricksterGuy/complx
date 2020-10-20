c2a1d586b6c5c225e0697352e7232818da0a5c5b
Brandon <tricksterguy87@gmail.com>
2020-10-20 14:58:25 -0700

    Enclode invalid instruction assembler error message with "'s

--- liblc3/lc3_assemble.cpp
+++ liblc3/lc3_assemble.cpp
@@ -87,7 +87,7 @@ std::string LC3AssembleException::what() const throw()
             snprintf(what_str, 1023, "Invalid register %s found on line %d", params[0].c_str(), lineno);
             break;
         case INVALID_INSTRUCTION:
-            snprintf(what_str, 1023, "Invalid instruction %s found on line %d", params[0].c_str(), lineno);
+            snprintf(what_str, 1023, "Invalid instruction \"%s\" found on line %d", params[0].c_str(), lineno);
             break;
         case INVALID_DIRECTIVE:
             snprintf(what_str, 1023, "Invalid assembler directive %s found on line %d", params[0].c_str(), lineno);
@@ -178,7 +178,7 @@ std::string LC3AssembleException::what() const throw()
             snprintf(what_str, 1023, "Invalid register %s", params[0].c_str());
             break;
         case INVALID_INSTRUCTION:
-            snprintf(what_str, 1023, "Invalid instruction %s", params[0].c_str());
+            snprintf(what_str, 1023, "Invalid instruction \"%s\"", params[0].c_str());
             break;
         case INVALID_FLAGS:
             snprintf(what_str, 1023, "Invalid condition code flags %s", params[0].c_str());
