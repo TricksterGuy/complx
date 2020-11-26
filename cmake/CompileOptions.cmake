c17e4715762d6867ce807db39016e9cff3a59b9c
Brandon <tricksterguy87@gmail.com>
2020-11-26 00:59:23 -0800

    update cginterals/cmake-init changes.

--- cmake/CompileOptions.cmake
+++ cmake/CompileOptions.cmake
@@ -1,7 +1,7 @@
 
-#
+# 
 # Platform and architecture setup
-#
+# 
 
 # Get upper case system name
 string(TOUPPER ${CMAKE_SYSTEM_NAME} SYSTEM_NAME_UPPER)
@@ -13,13 +13,13 @@ if(CMAKE_SIZEOF_VOID_P EQUAL 8)
 endif()
 
 
-#
+# 
 # Project options
-#
+# 
 
 set(DEFAULT_PROJECT_OPTIONS
-    #DEBUG_POSTFIX             ""
-    CXX_STANDARD              17 # Not available before CMake 3.1; see below for manual command line argument addition
+    DEBUG_POSTFIX             "d"
+    CXX_STANDARD              17
     LINKER_LANGUAGE           "CXX"
     POSITION_INDEPENDENT_CODE ON
     CXX_VISIBILITY_PRESET     "hidden"
@@ -27,23 +27,23 @@ set(DEFAULT_PROJECT_OPTIONS
 )
 
 
-#
+# 
 # Include directories
-#
+# 
 
 set(DEFAULT_INCLUDE_DIRECTORIES)
 
 
-#
+# 
 # Libraries
-#
+# 
 
 set(DEFAULT_LIBRARIES)
 
 
-#
+# 
 # Compile definitions
-#
+# 
 
 set(DEFAULT_COMPILE_DEFINITIONS
     SYSTEM_${SYSTEM_NAME_UPPER}
@@ -58,101 +58,106 @@ if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "MSVC")
 endif ()
 
 
-#
+# 
 # Compile options
-#
+# 
 
 set(DEFAULT_COMPILE_OPTIONS)
 
 # MSVC compiler options
 if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "MSVC")
     set(DEFAULT_COMPILE_OPTIONS ${DEFAULT_COMPILE_OPTIONS}
-        /MP           # -> build with multiple processes
-        /W4           # -> warning level 4
-        # /WX         # -> treat warnings as errors
+        PRIVATE
+            /MP           # -> build with multiple processes
+            /W4           # -> warning level 4
+            # /WX         # -> treat warnings as errors
 
-        /wd4251       # -> disable warning: 'identifier': class 'type' needs to have dll-interface to be used by clients of class 'type2'
-        /wd4592       # -> disable warning: 'identifier': symbol will be dynamically initialized (implementation limitation)
-        # /wd4201     # -> disable warning: nonstandard extension used: nameless struct/union (caused by GLM)
-        # /wd4127     # -> disable warning: conditional expression is constant (caused by Qt)
+            #$<$<CONFIG:Debug>:
+            #/RTCc         # -> value is assigned to a smaller data type and results in a data loss
+            #>
 
-        #$<$<CONFIG:Debug>:
-        #/RTCc         # -> value is assigned to a smaller data type and results in a data loss
-        #>
-
-        $<$<CONFIG:Release>:
-        /Gw           # -> whole program global optimization
-        /GS-          # -> buffer security check: no
-        /GL           # -> whole program optimization: enable link-time code generation (disables Zi)
-        /GF           # -> enable string pooling
-        >
-
-        # No manual c++11 enable for MSVC as all supported MSVC versions for cmake-init have C++11 implicitly enabled (MSVC >=2013)
+            $<$<CONFIG:Release>: 
+            /Gw           # -> whole program global optimization
+            /GS-          # -> buffer security check: no 
+            /GL           # -> whole program optimization: enable link-time code generation (disables Zi)
+            /GF           # -> enable string pooling
+            >
+            
+            /std:c++17
+        PUBLIC
+            /wd4251       # -> disable warning: 'identifier': class 'type' needs to have dll-interface to be used by clients of class 'type2'
+            /wd4592       # -> disable warning: 'identifier': symbol will be dynamically initialized (implementation limitation)
+            # /wd4201     # -> disable warning: nonstandard extension used: nameless struct/union (caused by GLM)
+            # /wd4127     # -> disable warning: conditional expression is constant (caused by Qt)
     )
 endif ()
 
 # GCC and Clang compiler options
 if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "GNU" OR "${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
     set(DEFAULT_COMPILE_OPTIONS ${DEFAULT_COMPILE_OPTIONS}
-        -Wall
-        -Wextra
-        -Wunused
+        PRIVATE
+            -Wall
+            -Wextra
+            -Wunused
 
-        -Wreorder
-        -Wignored-qualifiers
-        -Wmissing-braces
-        -Wreturn-type
-        -Wswitch
-        -Wswitch-default
-        -Wuninitialized
-        -Wmissing-field-initializers
-
-        $<$<CXX_COMPILER_ID:GNU>:
-            -Wmaybe-uninitialized
-
-            $<$<VERSION_GREATER:$<CXX_COMPILER_VERSION>,4.8>:
-                -Wpedantic
-
-                -Wreturn-local-addr
+            -Wreorder
+            -Wignored-qualifiers
+            -Wmissing-braces
+            -Wreturn-type
+            -Wswitch
+            -Wswitch-default
+            -Wuninitialized
+            -Wmissing-field-initializers
+            
+            $<$<CXX_COMPILER_ID:GNU>:
+                -Wmaybe-uninitialized
+                
+                $<$<VERSION_GREATER:$<CXX_COMPILER_VERSION>,4.8>:
+                    -Wpedantic
+                    
+                    -Wreturn-local-addr
+                >
             >
-        >
-
-        $<$<CXX_COMPILER_ID:Clang>:
-            -Wpedantic
-
-            # -Wreturn-stack-address # gives false positives
-        >
-
-        $<$<PLATFORM_ID:Darwin>:
-            -pthread
-        >
-
-        $<$<BOOL:${OPTION_COVERAGE_ENABLED}>:
-            -fprofile-arcs
-            -ftest-coverage
-        >
+            
+            $<$<CXX_COMPILER_ID:Clang>:
+                -Wpedantic
+                
+                # -Wreturn-stack-address # gives false positives
+            >
+            
+            $<$<BOOL:${OPTION_COVERAGE_ENABLED}>:
+                -fprofile-arcs
+                -ftest-coverage
+            >
+            -std=c++17
+        PUBLIC
+            $<$<PLATFORM_ID:Darwin>:
+                -pthread
+            >
     )
 endif ()
 
 
-#
+# 
 # Linker options
-#
+# 
 
 set(DEFAULT_LINKER_OPTIONS)
 
 # Use pthreads on mingw and linux
 if("${CMAKE_CXX_COMPILER_ID}" MATCHES "GNU" OR "${CMAKE_SYSTEM_NAME}" MATCHES "Linux")
     set(DEFAULT_LINKER_OPTIONS
-        ${DEFAULT_LINKER_OPTIONS}
-        -pthread
+        PUBLIC
+            ${DEFAULT_LINKER_OPTIONS}
+            -pthread
     )
-
+    
     if (${OPTION_COVERAGE_ENABLED})
         set(DEFAULT_LINKER_OPTIONS
-        ${DEFAULT_LINKER_OPTIONS}
-        -fprofile-arcs
-        -ftest-coverage
-    )
+            PUBLIC
+                ${DEFAULT_LINKER_OPTIONS}
+                -fprofile-arcs
+                -ftest-coverage
+        )
     endif ()
 endif()
